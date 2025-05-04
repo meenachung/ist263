958b0bfc0114bc22e01920430fb83a630c99c4cd
meenachung <mcchung@syr.edu>
2025-05-04 14:16:40 -0400

    project final test

--- project/project7/project.js
+++ project/project7/project.js
@@ -5,6 +5,8 @@ function hideNav () {
     document.getElementById("navlinks").style.display = "none";
 }
 
+
+
 function scrollGallery(direction) {
     const gallery = document.getElementById('gallery');
     const scrollAmount = 300; // pixels
